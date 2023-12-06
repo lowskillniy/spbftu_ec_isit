d15b8a10762294dab62fc3405893c648d4a5a122
Keernom <keernom.gdev@gmail.com>
2023-12-07 00:03:23 +0300

    Добавлена функция получения объёма услуг (Q)

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,9 @@
-function eval_proteins() {
-  return 42;
+/**
+ * @param   hours кол-во рабочих часов в сутках
+ * @param   days кол-во рабочих дней
+ * @param   hours стоимость одного часа работы
+ * @returns Объём услуг Q
+ */
+function getQ(hours, days, perHourCost) {
+  return +hours * +days * +perHourCost
 }
