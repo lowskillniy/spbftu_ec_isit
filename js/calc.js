a06027dc3c03f51030944882ff4d963431397aa3
Juwatomfri <Anyagavriliuk@gmail.com>
2023-12-11 21:23:29 +0300

    JS, в котором одна функция на обработку полей ввода

--- js/calc.js
+++ js/calc.js
@@ -7,3 +7,25 @@
 function getQ(hours, days, perHourCost) {
   return +hours * +days * +perHourCost
 }
+
+let getDataForCalc = () => {
+  let arrayOfForms = [...document.querySelectorAll(".inputForm")]
+  let arrayOfInputs = [...document.querySelectorAll(".dataInput")]
+  arrayOfForms.forEach((e) => {
+    e.addEventListener('submit', elem => {
+      elem.preventDefault()
+      console.log('it worked')
+      let allFilled = true
+      arrayOfInputs.forEach((element) => {
+        if (element.value == '') {
+          allFilled = false
+        }
+      })
+      if (!allFilled) {
+        alert('Необходимо заполнить все данные')
+      }
+    })
+  })
+}
+
+getDataForCalc()
