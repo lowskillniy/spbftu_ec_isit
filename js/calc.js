06eda3102c76877e5bb61231bfef4588b79e3949
Keernom <keernom.gdev@gmail.com>
2023-12-11 14:10:36 +0300

    feat: Добавлена функция расчета амортизационных затрат

--- js/calc.js
+++ js/calc.js
@@ -12,7 +12,7 @@ function getQ(hours, days, perHourCost) {
  * @param   equipmet приобретение оборудования (тыс.)
  * @param   capital первоначальные оборотные средства (тыс.)
  * @param   ownerCapital собственные средства предпринимателя (тыс.)
- * @returns Сумма, которую необходимо взять в кредит
+ * @returns Сумма, которую необходимо взять в кредит (тыс.)
  */
 function getCreditSum(equipmet, capital, ownerCapital) {
   return +equipmet * 1000 + +capital * 1000 - +ownerCapital * 1000
@@ -22,8 +22,17 @@ function getCreditSum(equipmet, capital, ownerCapital) {
  * @param   ownerSalary заработная плата бизнесмена (тыс.)
  * @param   workerSalary заработная плата работника (тыс.)
  * @param   percent страховые взносы ФЗП (процент 0-100)
- * @returns Сумма страховых взносов по расчету
+ * @returns Сумма страховых взносов по расчету (тыс.)
  */
 function getInsurancePayment(ownerSalary, workerSalary, percent) {
   return (+ownerSalary * 1000 + +workerSalary * 1000) * (+percent / 100)
+}
+
+/**
+ * @param   period амотризационный период оборудования (года)
+ * @param   equipmet стоимость приобретенного оборудования (тыс.)
+ * @returns Сумма амортизационных затрат (тыс.)
+ */
+function getAmortPayments(period, equipmet) {
+  return equipmet * 1000 / (period * 12)
 }
