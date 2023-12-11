562b4d8f7359f3e09b0ebc57830896b3da46d6b1
Keernom <keernom.gdev@gmail.com>
2023-12-11 13:58:15 +0300

    Добавлена функция подсчета суммы страховых взносов

--- js/calc.js
+++ js/calc.js
@@ -12,8 +12,18 @@ function getQ(hours, days, perHourCost) {
  * @param   equipmet приобретение оборудования
  * @param   capital первоначальные оборотные средства
  * @param   ownerCapital собственные средства предпринимателя
- * @returns сумма, которую необходимо взять в кредит
+ * @returns Сумма, которую необходимо взять в кредит
  */
 function getCreditSum(equipmet, capital, ownerCapital) {
   return +equipmet + +capital - +ownerCapital
+}
+
+/**
+ * @param   ownerSalary заработная плата бизнесмена (тыс.)
+ * @param   workerSalary заработная плата работника (тыс.)
+ * @param   percent страховые взносы ФЗП (процент 0-100)
+ * @returns Сумма страховых взносов по расчету
+ */
+function getInsurancePayment(ownerSalary, workerSalary, percent) {
+  return (+ownerSalary * 1000 + +workerSalary * 1000) * (+percent / 100)
 }
